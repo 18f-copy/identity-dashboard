9e324dbaefaf6cfef2bbf52ebe42f0a5fa167615
Oren Goldberg <ogoldberg@gmail.com>
2025-03-02 17:26:36 -0500

    Add app/controllers/users/omniauth_controller.rb

---
+++ app/controllers/users/omniauth_controller.rb
@@ -0,0 +1,22 @@
+module Users
+  class OmniauthController < ApplicationController
+    include TeamHelper
+
+    def callback
+      omniauth_info = request.env['omniauth.auth']['info']
+      @user = UserSession.new(omniauth_info).call
+
+      if @user && ( can_edit_teams?(@user) || can_create_teams?(@user) )
+        sign_in @user
+        store_id_token
+        redirect_to session[:requested_url]
+      else
+        redirect_to users_none_url
+      end
+    end
+
+    def store_id_token
+      session[:id_token] = request.env.dig('omniauth.auth', 'credentials', 'id_token')
+    end
+  end
+end
