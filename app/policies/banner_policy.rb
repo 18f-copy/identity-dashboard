a27f4b01b6f56bec6add266fb78b9d9ac09a7823
Oren Goldberg <ogoldberg@gmail.com>
2025-03-02 17:27:06 -0500

    Add app/policies/banner_policy.rb

---
+++ app/policies/banner_policy.rb
@@ -0,0 +1,17 @@
+class BannerPolicy < BasePolicy
+  def manage_banners?
+    admin?
+  end
+
+  def edit?
+    return false if record.ended? 
+    admin?
+  end
+
+  class Scope < BasePolicy::Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      user&.admin? ? scope.all : scope.none
+    end
+  end
+end
